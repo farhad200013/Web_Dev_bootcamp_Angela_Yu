53a1eb75da28ab46b02ca622d97a043358aa0e86
Farhad Gh <38926272+farhad200013@users.noreply.github.com>
2021-12-06 17:25:43 +0200

    Update 109.js

--- 109.js
+++ 109.js
@@ -1,4 +1,4 @@
 //Example code for 109. String Lengths and Retrieving the Number of Characters
 var tweet=prompt("Write your tweet here");
 var tweetCount=tweet.length;
-alert ("You have written "+tweetCount+ " Characters, you have "+(140-tweetCount)+" Characters remaining");
+alert ("You have written "+tweetCount+ " Characters, you have "+(140-tweetCount)+" Characters remaining.");
